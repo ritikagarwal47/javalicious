039e68bb72007347df8b006ea79f9e4d7346efec
Rahul Raj Tripathi <tripathirahul728@gmail.com>
2021-10-27 03:16:24 +0530

    Added GET_KPC Problqem

--- get_subsequence.java
+++ get_subsequence.java
@@ -1,5 +1,32 @@
 package javalicious;
-
-public class get_subsequence {
-
+import java.util.*;
+import java.io.*;
+public class get_subsequence
+{
+	public static void main(String[] args)throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		ArrayList<String> ar = findSubsequence(s);
+		System.out.println(ar);
+	}
+	public static ArrayList<String> findSubsequence(String s)
+	{
+		if(s.length()==0)
+		{
+			ArrayList<String> ot = new ArrayList<>();
+			ot.add("");
+			return ot;
+		}
+		String c = s.charAt(0)+"";
+		String ros = s.substring(1);
+		ArrayList<String> receive = findSubsequence(ros);
+		ArrayList<String> store = new ArrayList<>();
+		for(String rp : receive)
+		{
+			store.add(rp+"");
+			store.add(rp+c);
+		}
+		return store;
+	}
 }
